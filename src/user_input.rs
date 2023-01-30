38d26e0b5f463f6d8d66b3ab3872e79e4779c844
Guy Marshall <guymarshall9@gmail.com>
2023-01-30 22:17:30 +0000

    Upgrade get_user_input to return any type by using <T>

--- src/user_input.rs
+++ src/user_input.rs
@@ -1,13 +1,23 @@
 use std::io;
+use std::str::FromStr;
 
-pub fn get_user_input(prompt: &str) -> i32 {
+pub fn get_user_input<T>(prompt: &str) -> T
+    where
+        T: FromStr,
+        T::Err: std::fmt::Debug,
+{
     println!("{}", prompt);
 
     let mut user_input: String = String::new();
 
-    io::stdin().read_line(&mut user_input).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
 
-    let number: i32 = user_input.trim().parse().expect("Please enter an integer!");
+    let number: T = user_input
+        .trim()
+        .parse()
+        .expect("Please enter a valid number");
 
     return number;
 }
