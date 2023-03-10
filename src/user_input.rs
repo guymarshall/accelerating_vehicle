4f190c9bd87836a0e446d0002f3df433bf13ee72
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-10 12:18:38 +0000

    Change input from i32 to f64

--- src/user_input.rs
+++ src/user_input.rs
@@ -1,16 +1,16 @@
 use std::io;
 use std::error::Error;
 
-pub fn get_user_input(prompt: &str) -> Result<i32, Box<dyn Error>> {
+pub fn get_user_input(prompt: &str) -> Result<f64, Box<dyn Error>> {
     println!("{}", prompt);
 
     let mut user_input: String = String::new();
 
     io::stdin().read_line(&mut user_input)?;
 
-    let number: i32 = user_input.trim().parse().map_err(|_| "Please enter a valid integer!")?;
+    let number: f64 = user_input.trim().parse().map_err(|_| "Please enter a valid number!")?;
 
-    if number <= 0 {
+    if number <= 0.0 {
         return Err(From::from("The number must be greater than 0!"));
     }
 
