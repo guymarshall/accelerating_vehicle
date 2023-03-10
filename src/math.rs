055706ca8f0d8dbbfd344d69b8c5b9f3439adff7
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-10 12:24:44 +0000

    Remove commented and unnecessary commented java code

--- src/math.rs
+++ src/math.rs
@@ -1,18 +1,5 @@
 #![forbid(unsafe_code)]
 
-/*
-public static double mpsToMph(double speedInMps)
-{
-    return speedInMps * 3600.0 / 1600.0;
-}
-
-public static double input(String prompt)
-{
-    System.out.print(prompt);
-    return scanner.nextDouble();
-}
-*/
-
 pub fn mps_to_mph(speed_in_mps: f64) -> f64 {
     speed_in_mps * 3600.0 / 1600.0
 }
