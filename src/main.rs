a9a2ca8621c3142a9f90feecb20b089d9a08e785
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-10 12:27:32 +0000

    Convert start time and end time, and print time taken

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![forbid(unsafe_code)]
 
+use std::time::{Duration, Instant};
+
 mod user_input;
 mod math;
 
@@ -7,11 +9,15 @@ fn main() {
     println!("This program works out the top speed of a vehicle with a given mass and power output");
     let mass: f64 = user_input::input("Mass (kg):");
     let input_power: f64 = user_input::input("Power (W):");
+
+    let start_time: Instant = Instant::now();
+
+    let end_time: Instant = Instant::now();
+    let time_taken: Duration = end_time - start_time;
+    println!("This program took {:?} seconds to calculate.", time_taken);
 }
 
 /*
-long startTime = System.nanoTime();
-
 double airDensity = 1.225;
 double dragCoefficient = 0.3;
 double frontalArea = 0.85 * 1.89 * 1.46;
@@ -38,7 +44,4 @@ while (!finished)
     velocity = newVelocity;
     timeCounter += deltaT;
 }
-
-double timeTaken = (double)(System.nanoTime() - startTime) / 1_000_000_000;
-System.out.printf("This program took %f seconds to calculate.", timeTaken);
 */
