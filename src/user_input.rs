16259c4a10a586d02ecbb90fea0fba20c1c55da3
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-10 12:15:24 +0000

    Improve user_input.rs using code from csv_generator

--- src/user_input.rs
+++ src/user_input.rs
@@ -1,34 +1,31 @@
 use std::io;
-use std::str::FromStr;
+use std::error::Error;
 
-pub fn get_user_input<T>(prompt: &str) -> T
-    where
-        T: FromStr,
-        T::Err: std::fmt::Debug,
-{
+pub fn get_user_input(prompt: &str) -> Result<i32, Box<dyn Error>> {
     println!("{}", prompt);
 
     let mut user_input: String = String::new();
 
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Failed to read line");
+    io::stdin().read_line(&mut user_input)?;
 
-    let number: T = user_input
-        .trim()
-        .parse()
-        .expect("Please enter a valid number");
+    let number: i32 = user_input.trim().parse().map_err(|_| "Please enter a valid integer!")?;
 
-    return number;
+    if number <= 0 {
+        return Err(From::from("The number must be greater than 0!"));
+    }
+
+    Ok(number)
 }
 
-macro_rules! get_user_input {
-    ($prompt:expr, $T:ty) => {
-        {
-            println!("{}", $prompt);
-            let mut user_input = String::new();
-            std::io::stdin().read_line(&mut user_input).expect("Failed to read line");
-            user_input.trim().parse::<$T>().expect("Please enter a valid number")
-        }
+pub fn input(prompt: &str) -> i32 {
+    loop {
+        match get_user_input(prompt) {
+            Ok(count) => {
+                return count;
+            },
+            Err(error) => {
+                println!("Error: {}", error);
+            },
+        };
     }
 }
