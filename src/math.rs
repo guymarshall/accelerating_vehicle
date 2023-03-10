2705bec045b9aebf4249b194257b718e161e64af
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-10 12:20:56 +0000

    Add commented java to math.rs to convert

---
+++ src/math.rs
@@ -0,0 +1,12 @@
+/*
+public static double mpsToMph(double speedInMps)
+{
+    return speedInMps * 3600.0 / 1600.0;
+}
+
+public static double input(String prompt)
+{
+    System.out.print(prompt);
+    return scanner.nextDouble();
+}
+*/
