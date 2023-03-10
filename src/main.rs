17ff8a88d17e7e21df561cd81bf1d287f1df1db9
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-10 12:33:11 +0000

    Copy java code into loop for converting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![forbid(unsafe_code)]
 
 use std::time::{Duration, Instant};
+use crate::math::mps_to_mph;
 
 mod user_input;
 mod math;
@@ -16,11 +17,27 @@ fn main() {
     let drag_coefficient: f64 = 0.3;
     let frontal_area: f64 = 0.85 * 1.89 * 1.46;
 
-    let delta_t: i32 = 1;
+    let delta_t: f64 = 1.0;
 
-    let velocity: f64 = 0.0;
-    let time_counter: i32 = 1;
-    let finished: bool = false;
+    let mut velocity: f64 = 0.0;
+    let mut time_counter: f64 = 1.0;
+    let mut finished: bool = false;
+
+    while !finished {
+        let air_resistance_force: f64 = ((air_density * drag_coefficient * frontal_area) / 2.0) * velocity * velocity;
+        let delta_ke: f64 = delta_t * (input_power - (air_resistance_force * velocity));
+        let new_velocity: f64 = Math.sqrt(Math.pow(velocity, 2.0) + (2.0 * delta_ke / mass));
+
+        println!("Time: {} seconds, Velocity: {} mph%n", time_counter, mps_to_mph(new_velocity));
+
+        if new_velocity - velocity < 0.000001
+        {
+            finished = true;
+        }
+
+        velocity = new_velocity;
+        time_counter += delta_t;
+    }
 
     let end_time: Instant = Instant::now();
     let time_taken: Duration = end_time - start_time;
